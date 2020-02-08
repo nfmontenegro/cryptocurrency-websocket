9d2ca7b54d1176e3c7fe2de8bab5a3a81de25240
Nicolás Flores Montenegro <nicolas.fmontenegro@gmail.com>
2020-02-08 18:00:25 -0300

    condition to delete one user

--- api/src/controllers/user.js
+++ api/src/controllers/user.js
@@ -57,7 +57,13 @@ const deleteUser = (req, res) => {
 
   return prisma
     .deleteUser({id})
-    .then(user => res.status(200).json({data: user}))
+    .then(user => {
+      if (user) {
+        res.status(200).json({data: user})
+      } else {
+        res.status(404).json({message: 'User not found'})
+      }
+    })
     .catch(err => res.status(500).json({message: err.message}))
 }
 
