3dcb10b9dfaa11e27df6ecc94859046bce5f43e3
Nicolas Flores Montenegro <ext_niflores@falabella.cl>
2020-04-06 20:09:24 -0400

    fix persist session from redux-persist

--- frontend/src/utils/API.js
+++ frontend/src/utils/API.js
@@ -1,28 +1,16 @@
 import axios from 'axios'
-import {store} from '../redux/store'
 
 const API = axios.create({
   baseURL: 'http://localhost:3000/api/v1',
   responseType: 'json'
 })
 
-axios.interceptors.request.use(config => {
+API.interceptors.request.use(config => {
   const token = localStorage.getItem('token')
   if (token) {
     config.headers['Authorization'] = `Bearer ${token}`
   }
-  return token
+  return config
 })
-
-axios.interceptors.response.use(
-  response => response,
-  error => {
-    store.dispatch({
-      type: 'ERROR',
-      payload: error
-    })
-    return Promise.reject(error)
-  }
-)
 
 export default API
