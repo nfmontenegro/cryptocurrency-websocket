f5d6d76fb3e26a8fe75c0de1ed7aa71697ab65f7
Nicolás Flores Montenegro <nicolas.fmontenegro@gmail.com>
2020-07-29 19:12:18 -0400

    Revert "Revert "fix tests""
    This reverts commit 3a046ad297a08451a457cb02b13b7940ec59d468.

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -2,6 +2,7 @@ import {Response, Request, NextFunction} from "express";
 import {IErrorMessage} from "../interfaces";
 
 function errorHandler(error: IErrorMessage, _req: Request, res: Response, _next: NextFunction): object {
+  console.log("@@@ Main error :", error);
   return res.status(error.statusCode).send(error);
 }
 
